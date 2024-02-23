3852f039745530d900ba0d86f2e49f42169a3ee9
Brightwater <jeremiah.spears@sbcglobal.net>
2024-02-23 17:06:49 -0600

    Fixed project

---
+++ envLoader.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	GROUP_IRON_BASE_URL string `env:"GROUP_IRON_BASE_URL"`
+	GROUP_IRON_TOKEN    string `env:"GROUP_IRON_TOKEN"`
+	Port                string `env:"PORT"`
+}
+
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Error loading .env file, proceeding with environment variables if present.")
+		return nil, err
+	}
+
+	log.Println("Loading .env")
+	return &Config{
+		os.Getenv("GROUP_IRON_BASE_URL"),
+		os.Getenv("GROUP_IRON_TOKEN"),
+		os.Getenv("PORT"),
+	}, nil
+}
